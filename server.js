74dc31d30efa99180978c7e8340fe0622e0701ad
James Hahn <hahn.james.r@gmail.com>
2017-03-31 18:29:12 -0400

    further changes

--- server.js
+++ server.js
@@ -3,18 +3,21 @@
 // BASE SETUP
 // =============================================================================
 
-var mongoose   = require('mongoose');
+// db connection
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+
+var Bear = require('./app/models/bear');
 
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express');        // call express
+var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
@@ -23,9 +26,16 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware for all requests
+router.use(function (req, res, next) {
+    // log some shit
+    console.log('SOMETHING IS HAPPENING');
+    next(); // make sure we continue onto the next routes
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+router.get('/', function (req, res) {
+    res.json({message: 'hooray! welcome to ze api!'});
 });
 
 // more routes for our API will happen here
