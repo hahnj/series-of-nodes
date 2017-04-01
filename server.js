34bdc3467eba75c755c74113eec89275a5d9d82e
James Hahn <hahn.james.r@gmail.com>
2017-03-31 23:43:16 -0400

    added in the delete route

--- server.js
+++ server.js
@@ -95,6 +95,18 @@ router.route('/bears/:bear_id')
 
             res.json({message: 'Bear Updated!'});
         });
+    })
+
+    // kill the bear (DELETE http://localhost:8080/api/bears/:bear_id)
+    .delete(function (req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function (err, bear) {
+            if(err)
+                res.send(err);
+
+            res.json({messag: 'Successfully deleted'});
+        });
     });
 
 // REGISTER OUR ROUTES -------------------------------
